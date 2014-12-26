18b46d5f738adbedd9016a02b398ed4747401514
Ahmed Bhaila <ahmedajmal.bhaila@hyatt.com>
2014-12-26 14:38:03 -0600

    Using @SpringBootApplication annotation

--- src/main/java/com/mycompany/app/NonManagedBeansApplication.java
+++ src/main/java/com/mycompany/app/NonManagedBeansApplication.java
@@ -3,10 +3,8 @@ package com.mycompany.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.context.annotation.EnableLoadTimeWeaving.AspectJWeaving;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
@@ -68,9 +66,7 @@ class PersonFormatter {
 }
 
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableLoadTimeWeaving(aspectjWeaving=AspectJWeaving.ENABLED)
 @EnableSpringConfigured
 public class NonManagedBeansApplication {
